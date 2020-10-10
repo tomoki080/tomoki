3a1a2ae70d133fa0c3c4bd113a497a216f224356
tomoki080 <tomoki297@outlook.jp>
2020-10-11 01:04:19 +0900

    取り敢えず

--- kensaku/look.php
+++ kensaku/look.php
@@ -22,14 +22,12 @@
                     $rate = null;
                 }
 
-                if ( isset($_POST['play']) && is_array($_POST['play']) ) {
-                    $play1 = $_POST['play'];//この変数は配列
-                } elseif (isset($_POST['play'])) {
-                    $paly1 = $_POST['play'];
+                if ( isset($_POST['play']) ) {
+                    $play = $_POST['play'];
                 } else {
-                    $play1 = null;
+                    $play = null;
                 }
-                
+
                 if ( isset($_POST['done']) && is_array($_POST['done']) ) {
                     $done1 = $_POST['done'];//この変数は配列
                 } elseif ( isset($_POST['done']) ) {
@@ -69,71 +67,86 @@
                         $ratemin = $rate - 500;
                         $ratemax = $rate + 500;
                     }
-                    $sql .= "AND rate BETWEEN {$ratemin} AND {$ratemax} ";
-                }
-                if( isset($play1) ) {
-                    $sql .= "AND play IN (";
-                    foreach( $play1 as $key=>$value ) {
-                        $sql .= "$value";
-                        $sql .= isset($play1[$key + 1]) ? ",":"";//if(isset($play1[$key+1]) ? ",":;)
+                    if ( $level == null ) {
+                        $sql .= "rate BETWEEN {$ratemin} AND {$ratemax} ";
+                    } else {
+                        $sql .= "AND rate BETWEEN {$ratemin} AND {$ratemax} ";
                     }
-                    $sql .= ")";
+                }
+                if( $play == "両方" ) {
+                    if ( $level == null && $rate == null ) {
+                        $sql .= "play IN ('楽しく', '本気', '両方') ";
+                    } else {
+                        $sql .= "AND play IN ('楽しく', '本気', '両方') ";
+                    }
+                } else {
+                    if ( $level == null && $rate == null ) {
+                        $sql .= "play = '{$play}' ";
+                    } else {
+                        $sql .= "AND play = '{$play}' ";
+                    }
                 }
                 if( isset($done1)) {
-                    $sql .= "AND done IN (";
-                    foreach( $done1 as $key=>$value ) {
-                        $sql .= "$value";
+                    $sql .= "AND done IN ("; //条件分岐なし
+                    foreach($done1 as $key=>$value) {
+                        $sql .= "'{$value}'";　　// 二重で囲むと解決した(なんで)
                         $sql .= isset($done1[$key + 1]) ? ",":"";//if(isset($done1[$key+1]) ? ",":;)
                     }
                     $sql .= ")";
                 }
                 if ( $voice == "BOTH" ) {
-                    if ( $level == null && $rate == null && $play1 == null && $done1 == null ) {
+                    if ( $level == null && $rate == null && $play == null && $done1 == null ) {
                         $sql .= "voice IN ('YES', 'NO') ";
                     } else {
-                        $sql .= "AND voice IN ('YES', 'NO')";
+                        $sql .= "AND voice IN ('YES' , 'NO') ";
                     }    
                 } else {
-                    if ( $level == null && $rate == null && $play1 == null && $done1 == null ) {
-                        $sql .= "voice = {$voice}";
+                    if ( $level == null && $rate == null && $play == null && $done1 == null ) {
+                        $sql .= "voice = '{$voice}' ";
                     } else {
-                        $sql .= "AND voice = {$voice}";
+                        $sql .= "AND voice = '{$voice}' ";
                     }
                 }
                 
                 $stmt = $dbh->prepare($sql);
                 $stmt->execute();
+                //$test_stmt[] = $stmt->fetchAll(); //データが取得されているかの確認
 
                 $dbh = null;
 
-                echo '<table border="1" width="1000px">';
-                echo '<tr align="center">';
-                echo '<td>プレイヤーネーム</td>';
-                echo '<td>レベル</td>';
-                echo '<td>レート</td>';
-                echo '<td>プレイタイプ</td>';
-                echo '<td>行動タイプ</td>';
-                echo '<td>ボイスチャット</td>';
-                echo '</tr>';
-                while (true) {
-                    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
-                    if ($rec==false) {
-                        break;
-                    }
+                /*if ( empty($test_stmt[0]) ) {
+                    echo '該当するプレイヤーは存在しません。';
+                } else { */
+                    echo '<table border="1" width="1000px">';
                     echo '<tr align="center">';
-                    echo '<td>'.$rec['name'].'</td>';
-                    echo '<td>'.$rec['level'].'</td>';
-                    echo '<td>'.$rec['rate'].'</td>';
-                    echo '<td>'.$rec['play'].'</td>';
-                    echo '<td>'.$rec['done'].'</td>';
-                    echo '<td>'.$rec['voice'].'</td>';
-                    echo '<tr>';
-                }
-                echo '</table>';
+                    echo '<td>プレイヤーネーム</td>';
+                    echo '<td>レベル</td>';
+                    echo '<td>レート</td>';
+                    echo '<td>プレイタイプ</td>';
+                    echo '<td>行動タイプ</td>';
+                    echo '<td>ボイスチャット</td>';
+                    echo '</tr>';
+                    while (true) {
+                        $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+                        if ($rec==false) {
+                            break;
+                        }
+                        echo '<tr align="center">';
+                        echo '<td>'.$rec['name'].'</td>';
+                        echo '<td>'.$rec['level'].'</td>';
+                        echo '<td>'.$rec['rate'].'</td>';
+                        echo '<td>'.$rec['play'].'</td>';
+                        echo '<td>'.$rec['done'].'</td>';
+                        echo '<td>'.$rec['voice'].'</td>';
+                        echo '<tr>';
+                    }
+                    echo '</table>';
+                //}
 
             } catch(Exception $e) {
                 echo 'サーバーに障害が発生しています。';
                 echo 'しばらくお待ちください。';
+                echo $e;
                 exit();
             }
 
